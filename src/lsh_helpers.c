e6d3b551abcfa003f803bed8ef149998fe887994
Sebastiano <sebastiano@racca.me>
2024-01-31 17:40:35 +0100

    Add tilde expansion in the current working directory

--- src/lsh_helpers.c
+++ src/lsh_helpers.c
@@ -4,14 +4,25 @@
 #include <unistd.h>
 #include <errno.h>
 
+/**
+ * @brief Signal handler for SIGINT.
+ * @param signum The signal number.
+ */
 void sigint_handler(int signum) { }
 
-
+/**
+ * @brief Get the current username.
+ * @return A pointer to the current username string.
+ */
 char *get_current_username()
 {
     return getlogin();
 }
 
+/**
+ * @brief Get the hostname.
+ * @return A pointer to the hostname string.
+ */
 char *get_hostname()
 {
     char *hostname = (char *)malloc(256);
@@ -47,9 +58,40 @@ char *get_user_at_hostname()
     }
 }
 
+/**
+ * @brief Expand a tilde (~) in a path to the home directory.
+ * @param path The path containing a tilde (~).
+ * @return A pointer to the resulting string.
+ */
+char *expand_tilde(const char *path) {
+    const char *home = getenv("HOME");
+    if (home == NULL) {
+        fprintf(stderr, "Unable to get the home directory.\n");
+        return NULL;
+    }
 
-char *get_current_dir()
-{
+    char *result = NULL;
+    if (strstr(path, home) == path) {
+        size_t new_size = strlen(path) + strlen(home) + 1;
+        result = (char *)malloc(new_size);
+        if (result == NULL) {
+            perror("malloc");
+            return NULL;
+        }
+
+        snprintf(result, new_size, "~%s", path + strlen(home));
+    } else {
+        result = strdup(path);
+        if (result == NULL) {
+            perror("strdup");
+            return NULL;
+        }
+    }
+
+    return result;
+}
+
+char *get_current_dir() {
     char *cwd = NULL;
     size_t size = 1024; // Initial buffer size
 
@@ -61,7 +103,9 @@ char *get_current_dir()
         }
 
         if (getcwd(cwd, size) != NULL) {
-            return cwd;
+            char *expanded = expand_tilde(cwd);
+            free(cwd);
+            return expanded;
         } else if (errno == ERANGE) {
             // The buffer was too small; try again with a larger size.
             size *= 2;
